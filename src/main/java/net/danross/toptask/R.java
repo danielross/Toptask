88b7973f0c165ff8786338b4c3a83f6a75546369
Daniel Ross <daniel.je.ross@gmail.com>
2011-06-17 16:50:08 +0200

    xml strings and edit cleanup

--- src/main/java/net/danross/toptask/R.java
+++ src/main/java/net/danross/toptask/R.java
@@ -73,19 +73,16 @@ public final class R {
         public static final int app_name=0x7f040000;
         public static final int author=0x7f040002;
         public static final int body=0x7f040008;
-        public static final int category_prompt=0x7f04000d;
-        public static final int confirm=0x7f04000b;
-        public static final int date=0x7f040010;
-        public static final int edit_task=0x7f04000c;
-        public static final int end_prio=0x7f04000a;
-        public static final int end_priority_prompt=0x7f04000f;
+        public static final int category_prompt=0x7f04000a;
+        public static final int confirm=0x7f040009;
+        public static final int date=0x7f04000d;
+        public static final int end=0x7f04000c;
         public static final int no_tasks=0x7f040003;
-        public static final int start_prio=0x7f040009;
-        public static final int start_priority_prompt=0x7f04000e;
+        public static final int start=0x7f04000b;
         public static final int task_delete=0x7f040005;
         public static final int task_edit=0x7f040006;
         public static final int task_insert=0x7f040004;
-        public static final int time=0x7f040011;
+        public static final int time=0x7f04000e;
         public static final int title=0x7f040007;
     }
 }
