1a93e11ee21632acab1990d16ee29612c491d409
Daniel Ross <daniel.je.ross@gmail.com>
2011-06-20 19:20:33 +0200

    I did a booboo

--- project/plugins/plugins.scala
+++
@@ -1,6 +0,0 @@
-import sbt._
-
-class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
-    val android = "org.scala-tools.sbt" % "sbt-android-plugin" % "0.5.1"
-    val appenginePlugin = "net.stbbs.yasushi" % "sbt-appengine-plugin" % "2.2"
-}
